98943ad05333b222924451e2bbbe9786d7217a35
Jessica Nelson <yahwehschica@yahoo.com>
2021-12-02 13:39:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+class Polygon {
+    constructor(array) {
+        this.array = []
+        }
+   
+    get countSides() {
+       let sides = this.array.map(index => console.log(index))       
+      }
+
+    get perimeter() {
+        this.array.map(index => {
+            console.log(index)
+        })
+    }
+    // Use the get keyword to make a getter method countSides that 
+    // counts the number of sides (each index in the array).   
+}
+
+class Triangle extends Polygon {
+
+}
+
+
+class Square extends Polygon {
+
+}
